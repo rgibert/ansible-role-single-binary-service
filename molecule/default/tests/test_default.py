f89699b762ea95093aa971d0df2a688017c84ba5
Richard Gibert <richard@gibert.ca>
2019-11-09 20:28:46 -0500

    fix test cases

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_pip_installed(host):
-    assert host.package('python-pip').is_installed
+def file_installed(host):
+    assert host.file('/usr/local/share/prometheus_node_exporter-0.18.1/node_exporter-0.18.1.linux-amd64/node_exporter').is_file
